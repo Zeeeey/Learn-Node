56c81c2c3aca188aa2d4f47f4eff75a73438bd40
Zeeeey <ahumibe.ezinne@tizeti.com>
2020-04-19 00:53:50 +0100

    editflow for stores

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -20,3 +20,26 @@ exports.getStores = async (req, res) => {
     const stores = await Store.find();
     res.render('stores', { title: 'Stores', stores });
 }
+
+exports.editStore = async (req, res) => {
+    // 1. Find the store given the id
+    const store = await Store.findOne({_id: req.params.id});
+    // res.json(store);
+    // 2. confirm they are the owner of the store
+    // TODO
+    // 3. Render out the edit form so the user can update their store
+    res.render('editStore', { title: `Edit ${store.name}`, store });
+}
+
+exports.updateStore = async (req, res) => {
+    // find and updaate the store 
+    const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, 
+        {
+            new: true, // return the new store instead of the old one
+            runValidators: true // Update validators validate the update operation against the model's schema
+        }).exec();
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>.
+                <a href='/stores/${store.slug}'>View Store </a>`);
+    // Redirect them to the store and tell them it worked
+    res.redirect(`/stores/${store._id}/edit`);
+}
