a023da209f441deefc055a1e2a294f344366e10e
Zeeeey <ahumibe.ezinne@tizeti.com>
2020-04-26 16:15:53 +0100

    route and template of single store

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -78,3 +78,9 @@ exports.updateStore = async (req, res) => {
     // Redirect them to the store and tell them it worked
     res.redirect(`/stores/${store._id}/edit`);
 }
+
+exports.getStoreBySlug = async(req, res, next) => {
+    const store = await Store.findOne({ slug: req.params.slug })
+    if(!store) return next(); // if no valid store exists, render the 404 page i.e errorHandlers.js
+    res.render('store', {store, title: `Edit ${store.name}`})
+}
